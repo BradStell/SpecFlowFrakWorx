6e10cf28659c3e02231fa8a9ac54953b34b4bea4
Brad Stell <brad.stell@kingsmensoftware.com>
2016-03-07 14:10:42 -0500

    first commit; specflow working

---
+++ Login.feature
@@ -0,0 +1,15 @@
+﻿Feature: Login
+	In order to verify the login process functions correctly
+	As a user
+	I want to make sure the login process is working
+
+@login
+Scenario Outline: Login to frakworx2
+	Given I am at the login page
+	When I login with '<type>' credentials
+	Then I should be '<status>' logged into the program
+
+	Examples:
+		| type    | status         |
+		| valid   | successfully   |
+		| invalid | unsuccessfully |
---
+++ LoginSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace FrakWorx2SpecFlow.SpecFlow
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"I am at the login page")]
+        public void GivenIAmAtTheLoginPage()
+        {
+            Console.WriteLine("In login page");
+        }
+        
+        [When(@"I login with '(.*)' credentials")]
+        public void WhenILoginWithValidCredentials(string type)
+        {
+            Console.WriteLine("In when");
+        }
+        
+        [Then(@"I should be '(.*)' logged into the program")]
+        public void ThenIShouldBeSuccessfullyLoggedIntoTheProgram(string status)
+        {
+            Console.WriteLine("In then");
+        }
+    }
+}
